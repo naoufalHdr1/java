0d7a37e648288a1d72d2cafb1da6fe61d39b65ec
naoufalHdr1 <naoufalhadra96@gmail.com>
2025-02-06 10:54:24 +0100

    Write a Java program that prints the following output using System.out.println

---
+++ Println.java
@@ -0,0 +1,7 @@
+public class Println {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+        System.out.println("Let's learn printing in Java.");
+        System.out.println("Happy coding!");
+    }
+}
