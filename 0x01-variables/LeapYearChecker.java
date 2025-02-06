60673a695666b5862199def24154592680b0b3e8
naoufalHdr1 <naoufalhadra96@gmail.com>
2025-02-06 15:35:29 +0100

    Leap Year Checker

---
+++ 0x01-variables/LeapYearChecker.java
@@ -0,0 +1,11 @@
+public class LeapYearChecker {
+    public static void main(String[] args) {
+    	int year = 1996;
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
