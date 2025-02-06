78873ee2b7c5d1c1e371c5eed28f1a219d2d68e2
naoufalHdr1 <naoufalhadra96@gmail.com>
2025-02-06 15:38:20 +0100

    Find the average

---
+++ 0x01-variables/AverageCalculator.java
@@ -0,0 +1,8 @@
+public class AverageCalculator {
+    public static void main(String[] args) {
+    	double num1 = 10.0, num2 = 20.0, num3 = 30.0;
+        double average = (num1 + num2 + num3) / 3;
+
+        System.out.println("The average is: " + average);
+    }
+}
