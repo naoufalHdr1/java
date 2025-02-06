9c71b910343f9fcdb93076ac1b0a8f1daa3a62fd
naoufalHdr1 <naoufalhadra96@gmail.com>
2025-02-06 15:08:43 +0100

    Temperature Conversion

---
+++ 0x01-variables/TemperatureConversion.java
@@ -0,0 +1,9 @@
+public class TemperatureConversion {
+    public static void main(String[] args) {
+    	double celsius = 25.0;
+        double fahrenheit = (9.0 / 5.0) * celsius + 32;
+
+        System.out.println("Celsius: " + celsius);
+        System.out.println("Fahrenheit: " + fahrenheit);
+    }
+}
