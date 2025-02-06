ab6a08998e30c0002df178bb98c733fdb4428f08
naoufalHdr1 <naoufalhadra96@gmail.com>
2025-02-06 15:23:17 +0100

    Grade Calculation

---
+++ 0x01-variables/GradeCalculator.java
@@ -0,0 +1,17 @@
+public class GradeCalculator {
+    public static void main(String[] args) {
+    	int grade = 90;
+
+        if (grade >= 90) {
+            System.out.println("A");
+        } else if (grade >= 80) {
+            System.out.println("B");
+        } else if (grade >= 70) {
+            System.out.println("C");
+        } else if (grade >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
