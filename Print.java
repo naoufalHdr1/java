42a24adb6003fc58b2ba5fe9c35a4709dedb3e1f
naoufalHdr1 <naoufalhadra96@gmail.com>
2025-02-06 10:57:10 +0100

    Modify the program so that the output appears in a single line using 'System.out.print'

---
+++ Print.java
@@ -0,0 +1,7 @@
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("Hello, Java! ");
+        System.out.print("Let's learn printing in Java. ");
+        System.out.print("Happy coding!\n");
+    }
+}
