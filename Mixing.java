35f6bac811a604042cf1ff7a784a16315093d284
naoufalHdr1 <naoufalhadra96@gmail.com>
2025-02-06 10:58:51 +0100

    Mixing print and println

---
+++ Mixing.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Java is fun.\n");
+        System.out.print("Let's start coding.\n");
+        System.out.println("Welcome to Java!");
+    }
+}
