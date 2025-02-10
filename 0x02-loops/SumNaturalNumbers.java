4bc1d81400f6d0da3fdeae69f62100372a9094bc
naoufalHdr1 <naoufalhadra96@gmail.com>
2025-02-10 12:47:23 +0100

    Solution 2: Sum of Natural Numbers (While Loop)

---
+++ 0x02-loops/SumNaturalNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SumNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        int sum = 0, i = 1;
+
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
